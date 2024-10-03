6ba6d5f05b5db1e67062f1da579e38e0ff44fb50
Nadeem khan <khannadeem1892@gmail.com>
2024-10-03 11:15:14 +0530

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 require("./dataBase")
 const router = require("./maq-api/route")
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(cors())
 app.use("/", router);
 
 module.exports = app
